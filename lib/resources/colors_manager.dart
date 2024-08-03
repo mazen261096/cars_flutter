5b21703e144652b018c3a7f73a25cf800b1604ea
mazen <mazen261096@gmail.com>
2024-08-03 18:56:28 +0300

    first

---
+++ lib/resources/colors_manager.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class ColorsManager{
+  static Color primary = const Color(0xffeb802d);
+  static Color black = const Color(0xff222021);
+  static Color white = const Color(0xffffffff);
+}
